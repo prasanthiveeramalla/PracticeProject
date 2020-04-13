ef040a054d3dcea535b0a0f85c1c5b50bdee906e
bharath <bhartah@test.com>
2020-04-13 15:44:41 +0530

    added simple code

---
+++ TestProject/src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	
+	public static void main(String args[]) {
+		//System.out.print("testing");
+		
+		for(int i=1;i<=10000;i++) {
+			
+			if((i%2==1)&&(i%3==1)&&(i%4==1)&&(i%5==1)&&(i%6==1)&&(i%7==0)) {
+				System.out.print(i);
+				break;
+			}
+		}
+	}
+
+}
